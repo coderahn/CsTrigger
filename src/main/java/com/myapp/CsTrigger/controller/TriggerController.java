6998be3d6211485946aaa3d0cd95cc5217d8bc68
ash <ahnsh2748@gmail.com>
2024-11-14 21:33:48 +0900

    트리거 API 수정-이모지 완료 체크시 안 받아짐(슬랙 test 호출만 받음..)

--- src/main/java/com/myapp/CsTrigger/controller/TriggerController.java
+++ src/main/java/com/myapp/CsTrigger/controller/TriggerController.java
@@ -1,15 +1,32 @@
 package com.myapp.CsTrigger.controller;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @Slf4j
 public class TriggerController {
+    /**
+     * slack event 발생시 호출 API
+     * @param map
+     * @return
+     */
     @PostMapping("event/trigger")
-    public String test() {
-        log.info("trigger success");
-        return "success";
+    public ResponseEntity<Map> test(@RequestBody Map<String, String> map) {
+        //key&value printing
+        for (String key : map.keySet()) {
+            log.info("key:{}, value:{}", key, map.get(key));
+        }
+
+        Map<String, String> responseBody = new HashMap<>();
+        responseBody.put("challenge", String.valueOf(map.get("challenge")));
+
+        return ResponseEntity.ok(responseBody);
     }
 }
